17fef2eb96e0d54cf6a263ab77dc10afd5fa1656
Grenkin1988 <kruglovgleb@gmail.com>
2020-04-16 21:17:46 +0300

    MAke it Solution

---
+++ PollingConsumer.Tests/PollingConsumerProperties.fs
@@ -0,0 +1,5 @@
+﻿namespace PollingConsumer.Tests
+
+module Say =
+    let hello name =
+        printfn "Hello %s" name
---
+++ PollingConsumer/PollingConsumer.fs
@@ -0,0 +1,22 @@
+﻿module Samples.PollingConsumer
+
+open System
+
+// Auxiliary types
+type MessageHandler = unit -> Timed<unit>
+
+// State date
+type ReadyData = Timed<TimeSpan list>
+
+type ReceivedMessageData = Timed<TimeSpan list * MessageHandler>
+
+type NoMessageData = Timed<TimeSpan list>
+
+// State
+type PollingConsumer =
+    | ReadyState of ReadyData
+    | ReceivedMessageState of ReceivedMessageData
+    | NoMessageState of NoMessageData
+    | StoppedState
+
+// Transitions
