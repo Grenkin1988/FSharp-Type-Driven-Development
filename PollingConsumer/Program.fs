8c088875465bd7ac03d70758ca5140c00a393a95
grenkin1988 <kruglovgleb@gmail.com>
2020-04-19 17:57:25 +0300

    course is finished

--- PollingConsumer/Program.fs
+++ PollingConsumer/Program.fs
@@ -1,14 +1,14 @@
 open System
 open Samples
+open Samples.ColorPrint
 
-[<EntryPoint>]
-let main args =
-    let clock = Clocks.machineClock
-    let timeAtEntry = clock()
-    let stopBefore = timeAtEntry + TimeSpan.FromMinutes 1.
+let limit = TimeSpan.FromMinutes 1.
+
+let composeTransition (now : DateTimeOffset) =
+    let stopBefore = now + limit
     let estimatedDuration = TimeSpan.FromSeconds 2.
     let idleDuration = TimeSpan.FromSeconds 5.
-
+    
     let calculateExpectedDuration =
         Statistics.calculateExpectedDuration estimatedDuration
     let shouldPoll = Imp.shouldPoll calculateExpectedDuration stopBefore
@@ -16,18 +16,51 @@ let main args =
     let r = Random ()
     let pollForMessage = Simulation.pollForMessage r
     let handle = Simulation.handle r
-    let poll = Imp.poll pollForMessage handle clock 
-    
-    let shouldIdle = Imp.shouldIdle idleDuration stopBefore
-    
-    let idle = Imp.idle idleDuration
-    
-    let transition =
-        PollingConsumer.transition shouldPoll poll shouldIdle idle 
+    let poll = Imp.poll pollForMessage handle Clocks.machineClock
 
-    printf "Program starting"
-    PollingConsumer.startOn clock
-    |> PollingConsumer.unfurl transition
-    |> PollingConsumer.run
-    |> ignore
+    let shouldIdle = Imp.shouldIdle idleDuration stopBefore
+
+    let idle = Imp.idle idleDuration
+
+    PollingConsumer.transition shouldPoll poll shouldIdle idle
+
+let printOnEntry (timeAtEntry : DateTimeOffset) =
+    printfn "Started polling at %s." (timeAtEntry.ToString "T")
+    printfn ""
+
+let printOnExit timeAtEntry (durations : TimeSpan list) =
+    let stats = Statistics.calculateAverageAndStandardDeviation durations
+
+    let timeAtExit = DateTimeOffset.Now
+    let elapsed = timeAtExit - timeAtEntry
+    let durationColor =
+        if elapsed <= limit then ConsoleColor.Green else ConsoleColor.Red
+
+    printfn ""
+    printfn "Stopped polling at %s." (timeAtExit.ToString "T")
+    printf  "Elapsed time: "
+    cprintf durationColor "%s" (elapsed.ToString "c")
+    printfn "."
+    printfn "Handled %i message(s)." durations.Length
+    stats
+    |> Option.map (fun (avg, stdDev) -> avg.ToString "T", stdDev.ToString "T")
+    |> Option.iter (fun (avg, stdDev) ->
+        printfn "Average duration: %s" avg
+        printfn "Standard deviation: %s" stdDev)
+
+[<EntryPoint>]
+let main args =
+    let clock = Clocks.machineClock
+    let timeAtEntry = clock()
+
+    printOnEntry timeAtEntry
+
+    let durations =
+        PollingConsumer.startOn Clocks.machineClock
+        |> PollingConsumer.unfurl (composeTransition timeAtEntry)
+        |> PollingConsumer.run
+        |> PollingConsumer.durations
+    
+    printOnExit timeAtEntry durations
+    
     0
