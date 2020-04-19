ebe7c859bd0cdba6c772a107f2357346814b899a
grenkin1988 <kruglovgleb@gmail.com>
2020-04-19 16:45:49 +0300

    one more property

--- PollingConsumer.Test/StateMachineTests.fs
+++ PollingConsumer.Test/StateMachineTests.fs
@@ -5,72 +5,65 @@ open Samples
 open Samples.PollingConsumer
 open NUnit.Framework
 open FsCheck
+open FsCheck.NUnit
 open System
 
 [<TestFixture>]
 module StateMachineTests =
-    [<Test>]
-    let ``unfurl returns correct seq with constant transitions`` () =
-        let test 
-            (initial: string)
-            (constantValue: string)
-            (count: byte) = 
-            let getNext _ = constantValue
-            let actual : string seq = unfurl getNext initial
-            test <@
-                    actual
-                    |> Seq.skip 1
-                    |> Seq.truncate (int count)
-                    |> Seq.forall ((=) constantValue) @>
+    [<Property>]
+    let ``unfurl returns correct seq with constant transitions``
+        (initial: string)
+        (constantValue: string)
+        (count: byte) = 
+        let getNext _ = constantValue
+        let actual : string seq = unfurl getNext initial
+        test <@
+                actual
+                |> Seq.skip 1
+                |> Seq.truncate (int count)
+                |> Seq.forall ((=) constantValue) @>
 
-        Check.QuickThrowOnFailure test
+    [<Property>]
+    let ``unfurl returns as many values as requested``
+        (initial: TimeSpan)
+        (count: byte) = 
+        let count = int count
+        let actual = unfurl id initial
+        let actual = actual |> Seq.truncate count |> Seq.length
+        count =! actual
 
-    [<Test>]
-    let ``unfurl returns as many values as requested`` () =
-        let test 
-            (initial: TimeSpan)
-            (count: byte) = 
-            let count = int count
-            let actual = unfurl id initial
-            let actual = actual |> Seq.truncate count |> Seq.length
-            count =! actual
-
-        Check.QuickThrowOnFailure test
-
-    [<Test>]
-    let ``unfurl returns correct values`` () =
-        let test 
-            (initial: byte)
-            (count: byte) = 
-            let count = int count
-            let initial = int initial
-            let actual = 
-                unfurl ((+) 1) initial
-                |> Seq.truncate (count + 1) 
-                |> Seq.toList
-            let expected = 
-                [initial .. initial + count]
-            expected =! actual
-
-        Check.QuickThrowOnFailure test
+    [<Property>]
+    let ``unfurl returns correct values``
+        (initial: byte)
+        (count: byte) = 
+        let count = int count
+        let initial = int initial
+        let actual = 
+            unfurl ((+) 1) initial
+            |> Seq.truncate (count + 1) 
+            |> Seq.toList
+        let expected = 
+            [initial .. initial + count]
+        expected =! actual
 
     type WithoutStoppedStste =
         static member State() =
             Arb.Default.Derive()
             |> Arb.filter (not << isStopped)
 
-    [<Test>]
-    let ``run returns last element of seq without stops`` () =
-        let test (states: State list) =
-            not states.IsEmpty ==> lazy
-            
-            let actual : State = run states
-            let expected = states |> Seq.last
-            expected =! actual
+    [<Property(Arbitrary = [| typeof<WithoutStoppedStste> |])>]
+    let ``run returns last element of seq without stops``
+        (states: State list) =
+        not states.IsEmpty ==> lazy
+        
+        let actual : State = run states
+        let expected = states |> Seq.last
+        expected =! actual
 
-        let config = Config.QuickThrowOnFailure
-        let config = { config with Arbitrary = [ typeof<WithoutStoppedStste>] }
-        Check.One (config,test)
-
-
-
+    [<Property>]
+    let ``run returns StoppedState when it occures``
+        (states : State list) =
+        states |> List.exists isStopped ==> lazy
+        
+        let actual = run states
+        test <@ isStopped actual @>
